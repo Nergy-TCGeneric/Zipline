8f43e90bd3ba2daf8c9d20a22eecfc0e88c92653
Nergy-TCGeneric <5158870+Nergy-TCGeneric@users.noreply.github.com>
2022-09-11 21:01:21 +0900

    impl: Baekjoon HTML problem list parser

--- baekjoon_tui.py
+++ baekjoon_tui.py
@@ -17,20 +17,18 @@ class ProblemCategory:
     solved_count: int = -1
 
 @dataclass
-class ProblemList:
-    ids: list[int] = field(default_factory=list)
-    titles: list[str] = field(default_factory=list)
-    labels: list[str] = field(default_factory=list)
-    accepted_submits: list[int] = field(default_factory=list)
-    submits: list[int] = field(default_factory=list)
+class ProblemPreview:
+    id: int = -1
+    title: str = ''
+    labels: str = ''
+    accepted_submits: int = -1
+    submits: int = -1
 
 @dataclass
 class Problem:
-    id: int = -1
-    title: str = ''
+    preview: ProblemPreview
     time_limit: int = -1
     memory_limit: int = -1 # in kbyte
-    accepted_submits: int = -1
     description: str = ''
     input_desc: str = ''
     output_desc: str = ''
@@ -56,7 +54,7 @@ class ProblemCategoryParser(HTMLParser):
             0    1      2             3         4           5
             id   title  description   (unused)  total_cnt   solved
         '''
-        if self.is_parsing_table != True:
+        if not self.is_parsing_table:
             return
 
         if self.table_index == 0:
@@ -85,6 +83,62 @@ class ProblemCategoryParser(HTMLParser):
 
     def get_all_problem_category(self) -> list[ProblemCategory]:
         return self.categories
+
+class ProblemListParser(HTMLParser):
+    problems: list[ProblemPreview] = []
+    current_preview: ProblemPreview
+    is_parsing_table: bool = False
+    is_parsing_description: bool = False
+    table_index: int = 0
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "tbody":
+            self.is_parsing_table = True
+        if tag == "tr" and self.is_parsing_table:
+            self.current_preview = ProblemPreview()
+            self.table_index = 0
+
+    def handle_data(self, data):
+        '''
+            0          1           2       3                4          5        6
+            (unused)   problem_id  title   label(optional)  accepted   submits  ratio
+        '''
+        if not self.is_parsing_table:
+            return
+        
+        # FIXME: Not this time. Will be implemented soon
+        if self.is_parsing_description:
+            return
+        
+        if self.table_index == 1:
+            self.current_preview.id = int(data.rstrip())
+        elif self.table_index == 2:
+            self.current_preview.title = data
+        elif self.table_index == 3:
+            self.current_preview.labels = data
+        elif self.table_index == 4:
+            self.current_preview.accepted_submits = int(data.rstrip())
+        elif self.table_index == 5:
+            self.current_preview.submits = int(data.rstrip())
+        elif self.table_index > 6:
+            raise ValueError('Tried to parse table over boundary')
+
+    def handle_endtag(self, tag):
+        if tag == "tbody":
+            self.is_parsing_table = False
+        if tag == "td" and self.is_parsing_table:
+            self.table_index += 1
+        # Oddly, all end tags are intact in problem list page. oh, those inconsistencies..
+        if tag == "tr":
+            if not self.is_parsing_description:
+                self.problems.append(self.current_preview)
+            self.is_parsing_description = not self.is_parsing_description
+
+    def get_Nth_problem_preview(self, N:int) -> ProblemPreview:
+        return self.problems[N]
+    
+    def get_all_problem_previews(self) -> list[ProblemPreview]:
+        return self.problems
 
 class ProblemParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
--- test_problem_fetching.py
+++ test_problem_fetching.py
@@ -1,16 +1,21 @@
 import unittest
 import requests
-from baekjoon_tui import ProblemCategoryParser, ProblemParser, ProblemCategory
+from baekjoon_tui import *
 
 class ParserTestCase(unittest.TestCase):
     category_parser: ProblemCategoryParser
+    plist_parser: ProblemListParser
 
     def setUp(self):
-        req = requests.get("https://acmicpc.net/step")
+        category_req = requests.get("https://acmicpc.net/step")
         self.category_parser = ProblemCategoryParser()
-        self.category_parser.feed(req.text)
+        self.category_parser.feed(category_req.text)
 
-    def test_should_extract_one_essential_datum_from_table(self):
+        plist_req = requests.get("https://acmicpc.net/step/1")
+        self.plist_parser = ProblemListParser()
+        self.plist_parser.feed(plist_req.text)
+
+    def test_should_get_one_problem_category(self):
         # Extracting very first category
         first = self.category_parser.get_Nth_problem_category(0)
 
@@ -19,13 +24,30 @@ class ParserTestCase(unittest.TestCase):
         self.assertEqual(first.title, "입출력과 사칙연산")
         self.assertEqual(first.total_count, 15)
 
-    def test_should_extract_all_essential_data_from_table(self):
+    def test_should_get_all_problem_categories(self):
         all_category = self.category_parser.get_all_problem_category()
         for category in all_category:
             self.assertGreater(category.id, -1)
             self.assertNotEqual(category.title, '')
             self.assertNotEqual(category.description, '')
             self.assertGreaterEqual(category.total_count, -1)
+
+    def test_should_get_one_problem_list(self):
+        # Extracting very first problem list as did
+        first = self.plist_parser.get_Nth_problem_preview(0)
+
+        self.assertEqual(first.id, 2557)
+        self.assertEqual(first.title, "Hello World")
+        self.assertGreater(first.accepted_submits, 0)
+        self.assertGreater(first.submits, 0)
+
+    def test_should_get_all_problem_list(self):
+        all_problems = self.plist_parser.get_all_problem_previews()
+        for preview in all_problems:
+            self.assertGreater(preview.id, 999)
+            self.assertNotEqual(preview.title, '')
+            self.assertGreater(preview.accepted_submits, -1)
+            self.assertGreater(preview.submits, -1)
 
 if __name__ == "__main__":
     unittest.main()
