6ca579fe74cc962a0d16680bc175e8a95c465247
Nergy-TCGeneric <5158870+Nergy-TCGeneric@users.noreply.github.com>
2022-10-16 21:01:59 +0900

    fix: Add extensive tests for languages
    DESCRIPTION
    
    * Added testcases for Language.get_file_extension().
    -> This checks whether result is same as BOJ's.

--- zipline.py
+++ zipline.py
@@ -148,6 +148,7 @@ class Language(IntEnum):
     Java8_OpenJDK = 91
     Java11 = 93
     Java15 = 107
+
     Kotlin_JVM = 69
     Kotlin_Native = 92
 
@@ -157,6 +158,7 @@ class Language(IntEnum):
 
     D = 29
     D_LDC = 100
+
     FSharp = 108
     FSharp_Mono = 37
 
@@ -167,16 +169,10 @@ class Language(IntEnum):
     Go = 12
     Go_GCC = 90
 
-    Swift = 74
-    Objective_C = 10
-    Objective_CPP = 64
-
-    PHP = 7
     NodeJs = 17
     TypeScript = 106
     Rhino = 34
 
-    FreeBASIC = 78
     VB_NET_2_Mono = 20
     VB_NET_4_Mono = 63
     VisualBasic = 109
@@ -184,6 +180,7 @@ class Language(IntEnum):
     Assembly_32bit = 27
     Assembly_64bit = 87
 
+    PHP = 7
     Haskell = 11
     Text = 58
     GolfScript = 79
@@ -204,6 +201,10 @@ class Language(IntEnum):
     Pike = 41
     sed = 43
     Boo = 46
+    FreeBASIC = 78
+    Swift = 74
+    Objective_C = 10
+    Objective_CPP = 64
     INTERCAL = 47
     bc = 48
     Nemerle = 53
@@ -219,6 +220,167 @@ class Language(IntEnum):
     APECODE = 110
     Crystal = 111
     Umjunsik = 112
+
+    def get_file_extension(self) -> str:
+        if self in [
+            Language.C90,
+            Language.CLANG_C90,
+            Language.C99,
+            Language.CLANG_C99,
+            Language.C11,
+            Language.CLANG_C11,
+            Language.C2x,
+            Language.CLANG_C2x,
+        ]:
+            return ".c"
+
+        elif self in [
+            Language.CPP98,
+            Language.CLANG_CPP98,
+            Language.CPP11,
+            Language.CLANG_CPP11,
+            Language.CPP14,
+            Language.CLANG_CPP14,
+            Language.CPP17,
+            Language.CLANG_CPP17,
+            Language.CPP20,
+            Language.CLANG_CPP20,
+        ]:
+            return ".cc"
+
+        elif self in [Language.CSharp, Language.CSharp_3_Mono, Language.CSharp_6_Mono]:
+            return ".cs"
+
+        elif self in [
+            Language.Python2,
+            Language.Python3,
+            Language.PyPy2,
+            Language.PyPy3,
+            Language.Haxe,
+        ]:
+            return ".py"
+
+        elif self in [
+            Language.Java8,
+            Language.Java8_OpenJDK,
+            Language.Java11,
+            Language.Java15,
+        ]:
+            return ".java"
+
+        elif self in [Language.Kotlin_JVM, Language.Kotlin_Native]:
+            return ".kt"
+
+        elif self in [Language.Rust_2015, Language.Rust_2018, Language.Rust_2021]:
+            return ".rs"
+
+        elif self in [Language.D, Language.D_LDC]:
+            return ".d"
+
+        elif self in [Language.FSharp, Language.FSharp_Mono]:
+            return ".fs"
+
+        elif self in [Language.Ruby, Language.Ruby_18, Language.Ruby_19]:
+            return ".rb"
+
+        elif self in [Language.Go, Language.Go_GCC]:
+            return ".go"
+
+        elif self in [Language.NodeJs, Language.TypeScript, Language.Rhino]:
+            return ".js"
+
+        elif self in [
+            Language.VB_NET_2_Mono,
+            Language.VB_NET_4_Mono,
+            Language.VisualBasic,
+        ]:
+            return ".vb"
+
+        elif self in [Language.Assembly_32bit, Language.Assembly_64bit]:
+            return ".asm"
+
+        elif self == Language.PHP:
+            return ".php"
+        elif self == Language.Haskell:
+            return ".hs"
+        elif self == Language.Text:
+            return ".txt"
+        elif self == Language.GolfScript:
+            return ".gs"
+        elif self == Language.Pascal:
+            return ".pas"
+        elif self == Language.Scala:
+            return ".scala"
+        elif self == Language.Lua:
+            return ".lua"
+        elif self == Language.Perl:
+            return ".pl"
+        elif self == Language.Bash:
+            return ".sh"
+        elif self == Language.Fortran:
+            return ".f95"
+        elif self == Language.Scheme:
+            return ".scm"
+        elif self == Language.Ada:
+            return ".ada"
+        elif self == Language.awk:
+            return ".awk"
+        elif self == Language.OCaml:
+            return ".ml"
+        elif self == Language.BrainFuck:
+            return ".bf"
+        elif self == Language.Whitespace:
+            return ".ws"
+        elif self == Language.Tcl:
+            return ".tcl"
+        elif self == Language.Cobol:
+            return ".cob"
+        elif self == Language.Pike:
+            return ".pike"
+        elif self == Language.sed:
+            return ".sed"
+        elif self == Language.Boo:
+            return ".boo"
+        elif self == Language.FreeBASIC:
+            return ".bas"
+        elif self == Language.Swift:
+            return ".swift"
+        elif self == Language.Objective_C:
+            return ".m"
+        elif self == Language.Objective_CPP:
+            return ".mm"
+        elif self == Language.INTERCAL:
+            return ".i"
+        elif self == Language.bc:
+            return ".bc"
+        elif self == Language.Nemerle:
+            return ".n"
+        elif self == Language.Cobra:
+            return ".cobra"
+        elif self == Language.Nimrod:
+            return ".nim"
+        elif self == Language.Algol_68:
+            return ".a68"
+        elif self == Language.Befunge:
+            return ".bf"
+        elif self == Language.LOLCODE:
+            return ".lol"
+        elif self == Language.Aheui:
+            return ".aheui"
+        elif self == Language.Coq:
+            return ".v"
+        elif self == Language.Minecraft:
+            return ".mca"
+        elif self == Language.SystemVerilog:
+            return ".sv"
+        elif self == Language.APECODE:
+            return ".ape"
+        elif self == Language.Crystal:
+            return ".cr"
+        elif self == Language.Umjunsik:
+            return ".umm"
+
+        raise Exception(f"can't find corresponding file extension for language: {self}")
 
 
 @unique
--- zipline_test.py
+++ zipline_test.py
@@ -40,5 +40,179 @@ class PostRequestPreparationTestcase(unittest.TestCase):
             self.assertNotEqual(form.csrf_key, "")
 
 
+class LanguageFileExtensionEquivalenceTestcase(unittest.TestCase):
+    def check_if_languages_share_equal_extensions(
+        self, languages: list[Language], extension: str
+    ):
+        for lang in languages:
+            self.assertEqual(lang.get_file_extension(), extension)
+
+    def check_if_language_have_equal_extension(
+        self, language: Language, extension: str
+    ):
+        self.assertEqual(language.get_file_extension(), extension)
+
+    def test_all_c_variants_share_equal_file_extensions(self):
+        C_VARIANTS = [
+            Language.C90,
+            Language.CLANG_C90,
+            Language.C99,
+            Language.CLANG_C99,
+            Language.C11,
+            Language.CLANG_C11,
+            Language.C2x,
+            Language.CLANG_C2x,
+        ]
+
+        self.check_if_languages_share_equal_extensions(C_VARIANTS, ".c")
+
+    def test_all_cpp_variants_share_equal_file_extensions(self):
+        CPP_VARIANTS = [
+            Language.CPP98,
+            Language.CLANG_CPP98,
+            Language.CPP11,
+            Language.CLANG_CPP11,
+            Language.CPP14,
+            Language.CLANG_CPP14,
+            Language.CPP17,
+            Language.CLANG_CPP17,
+            Language.CPP20,
+            Language.CLANG_CPP20,
+        ]
+
+        self.check_if_languages_share_equal_extensions(CPP_VARIANTS, ".cc")
+
+    def test_all_csharp_variants_share_equal_file_extensions(self):
+        # NOTE: C# is compiled by following arguments in BOJ. Assuming it have a default file extension '.cs'.
+        # dotnet new console --force -o Main && dotnet publish Main --configuration Release --self-contained true --runtime linux-x64
+        CSHARP_VARIANTS = [
+            Language.CSharp,
+            Language.CSharp_3_Mono,
+            Language.CSharp_6_Mono,
+        ]
+
+        self.check_if_languages_share_equal_extensions(CSHARP_VARIANTS, ".cs")
+
+    def test_all_python_variants_share_equal_file_extensions(self):
+        PYTHON_VARIANTS = [
+            Language.Python2,
+            Language.Python3,
+            Language.PyPy2,
+            Language.PyPy3,
+            Language.Haxe,
+        ]
+
+        self.check_if_languages_share_equal_extensions(PYTHON_VARIANTS, ".py")
+
+    def test_all_java_variants_share_equal_file_extensions(self):
+        JAVA_VARIANTS = [
+            Language.Java8,
+            Language.Java8_OpenJDK,
+            Language.Java11,
+            Language.Java15,
+        ]
+
+        self.check_if_languages_share_equal_extensions(JAVA_VARIANTS, ".java")
+
+    def test_all_kotlin_variants_share_equal_file_extensions(self):
+        KOTLIN_VARIANTS = [Language.Kotlin_JVM, Language.Kotlin_Native]
+
+        self.check_if_languages_share_equal_extensions(KOTLIN_VARIANTS, ".kt")
+
+    def test_all_rust_variants_share_equal_file_extensions(self):
+        RUST_VARIANTS = [Language.Rust_2015, Language.Rust_2018, Language.Rust_2021]
+
+        self.check_if_languages_share_equal_extensions(RUST_VARIANTS, ".rs")
+
+    def test_all_d_variants_share_equal_file_extensions(self):
+        D_VARIANTS = [Language.D, Language.D_LDC]
+
+        self.check_if_languages_share_equal_extensions(D_VARIANTS, ".d")
+
+    def test_all_f_variants_share_equal_file_extensions(self):
+        # NOTE: Like C#, Assuming F# have a default file extension '.fs'.
+        # In BOJ, F# is compiled by following arguments:
+        # dotnet new console --language "F#" --force -o Main && dotnet publish Main --configuration Release --self-contained true --runtime linux-x64
+        F_VARIANTS = [Language.FSharp, Language.FSharp_Mono]
+
+        self.check_if_languages_share_equal_extensions(F_VARIANTS, ".fs")
+
+    def test_all_ruby_variants_share_equal_file_extensions(self):
+        RUBY_VARIANTS = [Language.Ruby, Language.Ruby_18, Language.Ruby_19]
+
+        self.check_if_languages_share_equal_extensions(RUBY_VARIANTS, ".rb")
+
+    def test_all_go_variants_share_equal_file_extensions(self):
+        GO_VARIANTS = [Language.Go, Language.Go_GCC]
+
+        self.check_if_languages_share_equal_extensions(GO_VARIANTS, ".go")
+
+    def test_all_javascript_variants_share_equal_file_extensions(self):
+        JS_VARIANTS = [Language.NodeJs, Language.TypeScript, Language.Rhino]
+
+        self.check_if_languages_share_equal_extensions(JS_VARIANTS, ".js")
+
+    def test_all_basic_variants_share_equal_file_extensions(self):
+        # NOTE: Assuming VisualBasic have a default file extension '.vb'
+        BASIC_VARIANTS = [
+            Language.VB_NET_2_Mono,
+            Language.VB_NET_4_Mono,
+            Language.VisualBasic,
+        ]
+
+        self.check_if_languages_share_equal_extensions(BASIC_VARIANTS, ".vb")
+
+    def test_all_assembly_variants_share_equal_file_extensions(self):
+        ASM_VARIANTS = [Language.Assembly_32bit, Language.Assembly_64bit]
+
+        self.check_if_languages_share_equal_extensions(ASM_VARIANTS, ".asm")
+
+    def test_all_other_languages_matches_their_own_file_extensions(self):
+        self.check_if_language_have_equal_extension(Language.PHP, ".php")
+        self.check_if_language_have_equal_extension(Language.Haskell, ".hs")
+        self.check_if_language_have_equal_extension(Language.Text, ".txt")
+        self.check_if_language_have_equal_extension(Language.GolfScript, ".gs")
+        self.check_if_language_have_equal_extension(Language.Pascal, ".pas")
+        self.check_if_language_have_equal_extension(Language.Scala, ".scala")
+        self.check_if_language_have_equal_extension(Language.Lua, ".lua")
+        self.check_if_language_have_equal_extension(Language.Perl, ".pl")
+        self.check_if_language_have_equal_extension(Language.Bash, ".sh")
+        self.check_if_language_have_equal_extension(Language.Fortran, ".f95")
+        self.check_if_language_have_equal_extension(Language.Scheme, ".scm")
+        self.check_if_language_have_equal_extension(Language.Ada, ".ada")
+        self.check_if_language_have_equal_extension(Language.awk, ".awk")
+        self.check_if_language_have_equal_extension(Language.OCaml, ".ml")
+        self.check_if_language_have_equal_extension(Language.Whitespace, ".ws")
+        self.check_if_language_have_equal_extension(Language.Tcl, ".tcl")
+        self.check_if_language_have_equal_extension(Language.Cobol, ".cob")
+        self.check_if_language_have_equal_extension(Language.Pike, ".pike")
+        self.check_if_language_have_equal_extension(Language.sed, ".sed")
+        self.check_if_language_have_equal_extension(Language.Boo, ".boo")
+        self.check_if_language_have_equal_extension(Language.FreeBASIC, ".bas")
+        self.check_if_language_have_equal_extension(Language.Swift, ".swift")
+        self.check_if_language_have_equal_extension(Language.Objective_C, ".m")
+        self.check_if_language_have_equal_extension(Language.Objective_CPP, ".mm")
+        self.check_if_language_have_equal_extension(Language.INTERCAL, ".i")
+        self.check_if_language_have_equal_extension(Language.bc, ".bc")
+        self.check_if_language_have_equal_extension(Language.Nemerle, ".n")
+        self.check_if_language_have_equal_extension(Language.Cobra, ".cobra")
+        self.check_if_language_have_equal_extension(Language.Nimrod, ".nim")
+        self.check_if_language_have_equal_extension(Language.Algol_68, ".a68")
+        self.check_if_language_have_equal_extension(Language.LOLCODE, ".lol")
+        self.check_if_language_have_equal_extension(Language.Aheui, ".aheui")
+        self.check_if_language_have_equal_extension(Language.Minecraft, ".mca")
+        self.check_if_language_have_equal_extension(Language.SystemVerilog, ".sv")
+        self.check_if_language_have_equal_extension(Language.APECODE, ".ape")
+        self.check_if_language_have_equal_extension(Language.Crystal, ".cr")
+        self.check_if_language_have_equal_extension(Language.Umjunsik, ".umm")
+
+        # NOTE: See https://stackoverflow.com/questions/7963528/what-does-v-stand-for-in-the-coq-file-extension, might be inaccurate
+        self.check_if_language_have_equal_extension(Language.Coq, ".v")
+
+        # NOTE: They are totally different, but accidentally they have a same extensions.
+        self.check_if_language_have_equal_extension(Language.BrainFuck, ".bf")
+        self.check_if_language_have_equal_extension(Language.Befunge, ".bf")
+
+
 if __name__ == "__main__":
     unittest.main()
